9f249f9a8e3be587c5cd2bb16226e57409118d75
johanvandongen <74316747+johanvandongen@users.noreply.github.com>
2023-06-04 19:47:07 +0200

    view doc data in html

--- src/components/ReadFile.tsx
+++ src/components/ReadFile.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import axios from 'axios';
+import { useState } from 'react';
 
 export interface IReadFileProps {
     token: string
 }
 
 export function ReadFile ({ token }: IReadFileProps) {
+    const [docBody, setDocBody] = useState<any>(null);
 
     function readFile(token: string) {
         const docId = '1JmQk0Gfto7J0Cax_S4Yz5X1_sEfZxAs1E5vbhf_cMK8'
@@ -13,7 +15,8 @@ export function ReadFile ({ token }: IReadFileProps) {
         axios.get(link, {
             headers: { 'Authorization': `Bearer ${token}` }
         }).then((response) => {
-            console.log(response.data)
+            console.log(response.data.body)
+            setDocBody(response.data.body)
         }).catch((error)=>{
             console.log(error)
         })
@@ -22,7 +25,21 @@ export function ReadFile ({ token }: IReadFileProps) {
   return (
     <div>
         <button onClick={() => readFile(token)}>read file</button>
-      {token}
+        {docBody !== null && docBody.content.map((el: any): JSX.Element | undefined => {
+            if (el.hasOwnProperty('table') ) {
+                console.log('table found', el.table.tableRows)
+                return <div style={{backgroundColor: 'gray', margin: '1rem'}}>table {el.table.tableRows.map((row: any) => {
+                    return <p>Table row{row.tableCells.map((cell: any) => {
+                        const cellContent = cell.content[0].paragraph.elements[0].textRun
+                        if (cellContent !== undefined) {
+                            return <p>{JSON.stringify(cellContent.content)}</p>
+                        }
+                    })}</p>
+                })}</div>
+                // return <p style={{marginTop: 50}}>{JSON.stringify(el.table)}</p>
+            }
+            return <div style={{paddingTop: 20}}></div>
+        })}
     </div>
   );
 }
