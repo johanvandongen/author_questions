6104cdb4031c7bfcbedb328f29b4322b18149c2c
johanvandongen <74316747+johanvandongen@users.noreply.github.com>
2023-06-10 13:02:37 +0200

    add comments

--- src/utils/googleDocParser.ts
+++ src/utils/googleDocParser.ts
@@ -2,11 +2,21 @@ import { v4 as uuidv4 } from 'uuid';
 import { type ITable } from '../models/ITable';
 import { parseTitle } from './utils';
 
-// https://developers.google.com/docs/api/samples/extract-text
+/**
+ * Returns the cell content of the second column.
+ * @param row row that you want the second cell content off.
+ */
+const getCellTextContent = (row: any): string[] => {
+    return readStructuralElements(row.tableCells[1].content);
+};
 
-const getCellTextContent = (cell: any): string[] => {
+/**
+ * Parses a structural element and returns a list of strings.
+ * @param elements structural element {@see https://developers.google.com/docs/api/samples/extract-text }
+ */
+const readStructuralElements = (elements: any): string[] => {
     const text: string[] = [];
-    for (const element of cell) {
+    for (const element of elements) {
         if (Object.prototype.hasOwnProperty.call(element, 'paragraph')) {
             const elements = element.paragraph.elements;
             for (const elem of elements) {
@@ -17,6 +27,10 @@ const getCellTextContent = (cell: any): string[] => {
     return text;
 };
 
+/**
+ * Returns the content of a paragraph.
+ * @param element paragraph element
+ */
 const parseParagraph = (element: any): string => {
     const hasTextRun = Object.prototype.hasOwnProperty.call(element, 'textRun');
     if (!hasTextRun) {
@@ -64,22 +78,22 @@ export const parseGoogleDoc = (doc: any): ITable[] | undefined => {
             issue: '',
             exerciseId: '',
             screenshot: undefined,
-            question: [''],
+            question: [],
             chapter: '',
             treated: '',
-            answer: [''],
-            authorReply: [''],
+            answer: [],
+            authorReply: [],
         };
         const rows: any[] = element.table.tableRows;
-        table.date = getCellTextContent(rows[0].tableCells[1].content)[0];
-        table.issue = getCellTextContent(rows[1].tableCells[1].content)[0];
-        table.exerciseId = getCellTextContent(rows[2].tableCells[1].content)[0];
-        table.screenshot = getCellTextContent(rows[3].tableCells[1].content);
-        table.question = getCellTextContent(rows[4].tableCells[1].content);
-        table.chapter = getCellTextContent(rows[5].tableCells[1].content)[0];
-        table.treated = getCellTextContent(rows[6].tableCells[1].content)[0];
-        table.answer = getCellTextContent(rows[7].tableCells[1].content);
-        table.authorReply = getCellTextContent(rows[8].tableCells[1].content);
+        table.date = getCellTextContent(rows[0])[0];
+        table.issue = getCellTextContent(rows[1])[0];
+        table.exerciseId = getCellTextContent(rows[2])[0];
+        table.screenshot = getCellTextContent(rows[3]);
+        table.question = getCellTextContent(rows[4]);
+        table.chapter = getCellTextContent(rows[5])[0];
+        table.treated = getCellTextContent(rows[6])[0];
+        table.answer = getCellTextContent(rows[7]);
+        table.authorReply = getCellTextContent(rows[8]);
 
         // No point of having a question info table with no question
         if (table.question.join() === '\n') {
